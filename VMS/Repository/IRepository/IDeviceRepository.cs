68820641808ac89d03dc46015251c230328f5eb7
melindamary <meldweb213@gmail.com>
2024-08-28 14:04:51 +0530

    update: modify the delete functionalities for device and visit purpose tables

--- VMS/Repository/IRepository/IDeviceRepository.cs
+++ VMS/Repository/IRepository/IDeviceRepository.cs
@@ -9,9 +9,11 @@ namespace VMS.Repository.IRepository
         Task<Device> AddDeviceAsync(AddNewDeviceDTO deviceDto);
         Task<IEnumerable<DeviceDTO>> GetDeviceListAsync();
 
-        Task<bool> DeleteDeviceAsync(int id);
+       /* Task<bool> DeleteDeviceAsync(int id);*/
 
         Task<bool> UpdateDeviceAsync(DeviceUpdateRequestDTO updateRequestDTO);
+
+        Task<bool> UpdateDeviceStatusAsync(DeviceStatusUpdateRequestDTO updateDeviceStatusRequestDTO);
         Task SaveAsync();
     }
 }
