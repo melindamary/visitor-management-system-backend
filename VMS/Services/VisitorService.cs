7783dad38bb01f4784d31d40e6abbff8e83f85b7
Adithya S Susa <adithya.susa@experionglobal.local>
2024-09-03 10:43:07 +0530

    modify: visitor service scheduled visitor method

--- VMS/Services/VisitorService.cs
+++ VMS/Services/VisitorService.cs
@@ -148,7 +148,8 @@ namespace VMS.Services
             try
             {
                 _logger.LogInformation("Fetching scheduled visitors for today.");
-                var scheduledVisitors = await _visitorRepository.GetVisitorLogs(v => v.Where(visitor => visitor.CheckInTime == null), locationName);
+                DateTime today = DateTime.Today;
+                var scheduledVisitors = await _visitorRepository.GetVisitorLogs(v => v.Where(visitor => visitor.VisitDate != today && visitor.CheckInTime == null), locationName);
                 _logger.LogInformation("Fetched {Count} scheduled visitors for today.", scheduledVisitors.Count());
                 return scheduledVisitors;
             }
