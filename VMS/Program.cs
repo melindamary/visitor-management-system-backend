14738265f0df7a34e213e94fe579dd46a4a80b2a
melindamary <meldweb213@gmail.com>
2024-11-20 18:44:15 +0530

    fix: modify dockerfile

--- VMS/Program.cs
+++ VMS/Program.cs
@@ -187,8 +187,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseCors("CorsPolicy");
+/*app.UseHttpsRedirection(); 
+*/app.UseCors("CorsPolicy");
 app.MapControllers();
 app.UseStaticFiles();
 
