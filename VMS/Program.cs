5f0ba890c4a4d40b1180310a3a245b7f4487ed81
melindamary <meldweb213@gmail.com>
2024-11-26 18:21:31 +0530

    update: update cors policy

--- VMS/Program.cs
+++ VMS/Program.cs
@@ -155,18 +155,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policy =>
     {
-        policy.SetIsOriginAllowed(origin =>
-        {
-            // Allow port 4200 specifically
-            if (origin == "http://localhost:4200")
-            {
-                return true;
-            }
-
-            // Allow any other port on localhost
-            Uri uri = new Uri(origin);
-            return uri.Host == "localhost";
-        })
+        policy
+        .AllowAnyOrigin()
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials(); // Use this cautiously
